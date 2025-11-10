677c6858248afe1ef094d752ea469e165ff5764f
Lydia Park <liapaark@gmail.com>
2025-11-09 20:24:30 -0600

    keeping the portion of the old history to "go back in time"

--- src/App.jsx
+++ src/App.jsx
@@ -55,15 +55,20 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length-1);
     setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
-    //todo
+    setCurrentMove(nextMove);
+      setXIsNext(nextMove % 2 === 0); 
+    }
   }
 
     const moves = history.map((squares, move) => {
